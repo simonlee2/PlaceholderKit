2ce0b6f3bf701d50d7b2b263fbc1db2c51e936b1
Simon <dev.simonlee@gmail.com>
2018-12-28 14:39:20 +0800

    Updated macOS example

--- Examples/ImagesExample-macOS/Sources/ImagesExample-macOS/main.swift
+++ Examples/ImagesExample-macOS/Sources/ImagesExample-macOS/main.swift
@@ -7,12 +7,7 @@
 import PlaceholderKit
 import Cocoa
 
-let images = [
-    Placeholder(size: CGSize(width: 100, height: 100), backgroundStyle: .solidColor(.red)),
-    Placeholder(size: CGSize(width: 200, height: 100), backgroundStyle: .solidColor(.blue)),
-    Placeholder(size: CGSize(width: 100, height: 200), backgroundStyle: .solidColor(.green)),
-    Placeholder(size: CGSize(width: 320, height: 480), backgroundStyle: .solidColor(.yellow)),
-].compactMap({$0.render()})
+let images = PlaceholderKit.defaultPlaceholders
 
 let desktopURL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
 
